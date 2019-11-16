07aebefaf9674557dc43842f7cfe916e965deb1e
Asad Chattha <15034198-079@uog.edu.pk>
2019-11-16 17:06:08 +0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Muhammad Asad Chattha on 16/11/2019.
+//  Copyright © 2019 Muhammad Asad Chattha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
